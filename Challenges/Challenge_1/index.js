dc21c0fab25729c13c82e5c1f8a8bafafec88b07
synxbroly <132735097+synxbroly@users.noreply.github.com>
2023-12-02 14:47:26 +0530

    added challenges folder

---
+++ Challenges/Challenge_1/index.js
@@ -0,0 +1,35 @@
+let marksWeight = 78;
+let marksHeight = 1.69;
+
+let johnWeight =92;
+let johnHeight =1.95;
+
+
+let bmiMark = marksWeight / marksHeight**2;
+let bmiJohn = johnWeight/johnHeight**2;
+
+console.log(bmiJohn, bmiMark)
+
+let markHigherBMI = bmiMark > bmiJohn;
+console.log(markHigherBMI);
+
+
+//2nd situation
+
+
+marksWeight = 95;
+marksHeight = 1.88;
+
+johnWeight =85;
+johnHeight =1.76;
+
+
+bmiMark = marksWeight / marksHeight**2;
+bmiJohn = johnWeight/johnHeight**2;
+
+console.log(bmiJohn, bmiMark)
+
+markHigherBMI = bmiMark > bmiJohn;
+console.log(markHigherBMI);
+
+
